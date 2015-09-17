a33727ebd8cb72c9a20f35f8946404fde5cd4550
Maximiliano Castro <fitolandia83@gmail.com>
2015-09-17 12:10:39 -0300

    [Maximiliano Castro] - 01-js-intro-and-jquery- completed  12, 13

--- 01-js-intro-and-jquery/Topic 1: JavaScript Intro and jQuery/js/script.js
+++ 01-js-intro-and-jquery/Topic 1: JavaScript Intro and jQuery/js/script.js
@@ -1,19 +1,17 @@
 /**
  * Created by maxi on 13/09/15.
  */
-$(document).on('ready', function(){
+$(document).on('ready', function() {
     var $section = $('.section');
-
-    $section.show(2000, function(){
+    $section.show(2000, function() {
        $('.alias').focus();
     });
-
-    $('.btn').on('click', function(){
+    $('.btn').on('click', function() {
         $.ajax({
             url: 'http://bootcamp.aws.af.cm/welcome/Maxi',
-            success: function(resp){
-                console.log(resp);
-                $section.html(resp.response);
+            success: function(response) {
+                console.log(response);
+                $section.html(response.response);
             },
             error: function(jqXHR, status, error) {
                 console.log(error);
